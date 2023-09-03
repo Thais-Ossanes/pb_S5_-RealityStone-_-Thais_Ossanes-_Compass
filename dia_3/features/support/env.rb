e640bbe46b7c568a336be34c3e522a059e7893a5
Thaís Ossanes <tf.ossanes@gmail.com>
2023-09-03 17:15:23 -0300

    alterações na develop

--- dia_3/features/support/env.rb
+++
@@ -1,7 +0,0 @@
-require 'cucumber'
-require 'selenium-webdriver'
-require 'rspec'
-require 'capybara/cucumber'
-
-Capybara.default_driver = :selenium_chrome
-Capybara.app_host = 'http://www.google.com.br'
